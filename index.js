4be2d9a9eff665ad2146e5c95af73fb75c44f688
Bjianyu <1968362785@qq.com>
2022-08-05 13:03:23 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let searchContent = document.querySelector("#search")
+function search() {
+    searchContent = searchContent.value;
+    window.location.href = `https://cn.bing.com/search?q=${searchContent}`;
+}
+
+// 搜素功能
+let searchContent = document.getElementById('searchContent');
+let searchButton = document.getElementById('searchButton');
 searchContent.addEventListener('keydown', (e) => {
-    if (e.key == 'Enter') {
-        searchContents = searchContent.value;
-        window.location.href = `https://cn.bing.com/search?q=${searchContents}`;
-    }
-})
+    if (e.key == 'Enter') { search() }
+});
+searchButton.addEventListener('click', search);
